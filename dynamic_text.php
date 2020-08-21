b96a4b9129b21f219a9d8fcec7664b8450f2121b
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-08-20 22:48:25 -0400

    update

--- dynamic_text.php
+++ dynamic_text.php
@@ -13,5 +13,6 @@ $tite = "Ratan Oberoi Official Site";
 <h1><?php echo $title; ?></h1>
 <h2><?php echo $title; ?></h2>
 <h3><?php echo $title; ?></h3>
+<h4><?php echo $title; ?></h4>
 </body>
 </html>
