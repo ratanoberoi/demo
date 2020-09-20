db4963af91d0c3ee0492c343a793b24494775f5a
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-09-20 19:47:44 -0400

    update

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$name = 'Ratan';
+$number = 100;
+$Number_List = 100;
+$name = "<h1> Hello </h1>"
+
+echo #name; 
+
+?>
+
+</body>
+</html>
