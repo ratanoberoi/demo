1239e17fc3ea97f7e9b29c45dd48c8c3ef210ac9
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-09-27 21:20:01 -0400

    update

---
+++ math.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 21 + 20;
+echo "<br>"
+echo 12 - 6;
+echo "<br>"
+echo 36 - 6 * 21;
+echo "<br>"
+echo 12 - 6;
+echo "<br>"
+$number1 = 12;
+$number1 = 24;
+
+echo $number1 = $number2;
+
+
+?>
+
+</body>
+</html>
