205c9f1e9fca5636ac993eb98b1037c37d82a0f5
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-09-28 22:47:23 -0400

    update

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$numberList = array(25,264,52,'99',348, '<h1>Hello</h1>');
+
+echo $numberList [0];
+
+?>
+
+</body>
+</html>
