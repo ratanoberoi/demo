0589b8bbba796e4858a1548f0cae977c310427fd
Ratan Oberoi <65130962+ratanoberoi@users.noreply.github.com>
2020-10-24 14:10:26 -0400

    update

---
+++ arrays_assoc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = array(10, 20, 30)
+
+echo $number [2] . "<br>";
+
+$names = array("first_name" => 'Ratan');
+
+?>
+
+
+
+</body>
+</html>
